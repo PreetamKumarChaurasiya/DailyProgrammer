34b70817ee484556fb3959b4b105410d535b4bea
Preetam Kumar Chaurasiya <122163552+PreetamKumarChaurasiya@users.noreply.github.com>
2023-01-09 14:57:21 +0530

    Added Challenge #1 Easy

---
+++ Challenge #1/Easy/challenge-1.py
@@ -0,0 +1,13 @@
+# Create a program that will ask the users name, age, and reddit username. 
+# Have it tell them the information back, in the format:
+# 
+# Your name is (blank), you are (blank) years old, and your username is (blank)
+# 
+# For extra credit, have the program log this information in a file to be accessed later.
+# 
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+reddit = input("What is your reddit username? ")
+
+print("Your name is " + name + ", you are " + age + " years old, and your username is " + reddit)
